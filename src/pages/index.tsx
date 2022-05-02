2fbca9fa8fcb03412f9f555f0ef7519caa60b11f
yubrot <yubrot@gmail.com>
2022-05-02 09:32:22 +0900

    Clean up

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <main>
+      <Head>
+        <title>Kusokollagen</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Kusokollagen</h1>
+    </main>
+  )
+}
+
+export default Home

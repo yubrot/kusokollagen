dc48cda5f1ea3fb61e7489340e1cd21958c76c5f
yubrot <yubrot@gmail.com>
2022-05-03 15:26:58 +0900

    backend: Specify Google Cloud Storage projectId explicitly

--- src/backend/google-cloud/storage.ts
+++ src/backend/google-cloud/storage.ts
@@ -1,6 +1,6 @@
 import { Storage } from '@google-cloud/storage';
 
 const credentials = JSON.parse(process.env.GOOGLE_CLOUD_STORAGE_KEY!);
-const storage = new Storage({ credentials });
+const storage = new Storage({ projectId: credentials.project_id, credentials });
 
 export default storage;
